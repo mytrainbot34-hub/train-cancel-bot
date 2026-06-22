432b758000b7dfe86c12e06553185b845d7b375c
mytrainbot34-hub <mytrainbot34@gmail.com>
2026-06-22 15:31:11 +0100

    Update train_cancel_bot.py

--- train_cancel_bot.py
+++ train_cancel_bot.py
@@ -16,8 +16,8 @@ import requests
 # ----------------------------------------------------------------------
 # Configuration
 # ----------------------------------------------------------------------
-ORIGIN = "MIA"               # Manchester Airport
-DESTINATION = "BIF"          # Barrow-in-Furness
+# ORIGIN = "MIA", "MAN", "PRE",        # Manchester Airport, #Manchester Picadilly, #Preston
+# DESTINATION = "BIF", "BPN", "SFD",   # Barrow-in-Furness,  #Blackpool North, #Salford Central
 START_TIME = time(6, 0)
 END_TIME = time(16, 0)
 
